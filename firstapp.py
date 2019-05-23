0e15d3761f9c910dc2693bdc664e06bf16b61ec9
Paul <pguise@cascadewebdev.com>
2019-05-22 21:59:30 -0700

    commented out and ready for next lesson

--- firstapp.py
+++ firstapp.py
@@ -103,25 +103,25 @@
 #    otherwise  
 #         its a lovely day
 
-is_hot = False
-is_cold = False
+#is_hot = False
+#is_cold = False
+#
+#if is_hot:
+#    print("It's hot outside.")
+#    print("Drink plenty of water.")
+#elif is_cold:
+#    print("It's a cold day")
+#    print("Wear warm clothes.")
+#else:
+#    print("It's a lovely day.")
+#print("Enjoy your day.") #not tabbed/indented statement will end an IF statement
 
-if is_hot:
-    print("It's hot outside.")
-    print("Drink plenty of water.")
-elif is_cold:
-    print("It's a cold day")
-    print("Wear warm clothes.")
-else:
-    print("It's a lovely day.")
-print("Enjoy your day.") #not tabbed/indented statement will end an IF statement
 
-
-good_credit = False
-house_price = 1000000
-if good_credit:
-    down_payment = house_price * 0.10
-else:
-    down_payment = house_price * 0.20
-
-print(f"Your down payment is $" + format(down_payment,",.2f") + " dollars.")
+#good_credit = False
+#house_price = 1000000
+#if good_credit:
+#    down_payment = house_price * 0.10
+#else:
+#    down_payment = house_price * 0.20
+#
+#print(f"Your down payment is $" + format(down_payment,",.2f") + " dollars.")
